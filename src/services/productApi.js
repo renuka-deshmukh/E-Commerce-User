430782544ecc821a4be84e1f3dd71008e58942e2
renuka-deshmukh <renuka.deshmukh776@gmail.com>
2025-10-16 23:43:41 +0530

    api

--- src/services/productApi.js
+++ src/services/productApi.js
@@ -5,5 +5,5 @@ export const getAllProducts = () => axiosInstance.get('/product/getAllProducts')
 export const getProductById = (id) => axiosInstance.get(`/product/getProductById/${id}`);
 
 export const getProductsByCategory = (catID, filters = {}) => {
-  return axiosInstance.get(`/product/category/${catID}`, { params: filters });
+  return axiosInstance.get(`/product/getProductByCategory/category/${catID}`, { params: filters });
 };
