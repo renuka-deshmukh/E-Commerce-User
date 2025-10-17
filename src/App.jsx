83bc74d7fb8766539f62ad7911faaa0d87f938e2
renuka-deshmukh <renuka.deshmukh776@gmail.com>
2025-10-17 19:36:59 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar'
 import Dashboard from './pages/user/Dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProductDetails from './pages/ProductDetails';
+import Footer from './components/Footer';
+import ProductsPage from './components/ProductsPage';
+import Login from './pages/authorization/Login';
+import Register from './pages/authorization/Register';
+import { AuthProvider } from './context/AuthProvider';
 
 function App() {
-
   return (
-    <>
-    <Navbar/>
-    <Dashboard/>
-      
-    </>
-  )
+    <AuthProvider>
+       <Navbar /> {/* Navbar can now access AuthContext */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/products/:category" element={<ProductsPage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
